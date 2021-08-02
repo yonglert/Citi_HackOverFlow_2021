29f5ca4512353791c6c5b6577e824fa3bd473801
Yong Ler <tyongler.tang@gmail.com>
2021-08-02 22:17:29 +0800

    fixed the overflow issue

--- citi_hackathon/lib/qrgen/qrgen.dart
+++ citi_hackathon/lib/qrgen/qrgen.dart
@@ -12,8 +12,6 @@ class QrGenPage extends StatelessWidget {
     return Scaffold(
 
         body: Container(
-          width: 500,
-          height: 500,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -22,31 +20,37 @@ class QrGenPage extends StatelessWidget {
               Image(image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQyYwscUPOH_qPPe8Hp0HAbFNMx-TxRFubpg&usqp=CAU")),
 
               //First Button
-              FlatButton(
-                padding: EdgeInsets.all(15),
-                onPressed: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ScanQR()));
-                },
-                child: Text("Scan QR Code",style: TextStyle(color: Colors.indigo[900]),),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  side: BorderSide(color: Colors.indigo[900]!),
+              Container(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.indigo,
+                      side: BorderSide(color: Colors.indigo, width: 2, )
+                  ),
+                  onPressed: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ScanQR()));
+                  },
+                  child: Text("Scan QR Code",
+                    style: TextStyle(
+                        color: Colors.indigo[900]),
+                  ),
                 ),
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
               ),
               SizedBox(height: 10),
-
               //Second Button
-              FlatButton(
-                padding: EdgeInsets.all(15),
-                onPressed: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=>
-                      GenerateQR()));
-                },
-                child: Text("Generate QR Code", style: TextStyle(color: Colors.indigo[900]),),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  side: BorderSide(color: Colors.indigo[900]!),
+              Container(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.indigo,
+                    side: BorderSide(color: Colors.indigo, width: 2)
+                  ),
+                  onPressed: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>
+                        GenerateQR()));
+                  },
+                  child: Text("Generate QR Code", style: TextStyle(color: Colors.indigo[900]),),
                 ),
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
               ),
             ],
           ),
