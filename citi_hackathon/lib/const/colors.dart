0fdd169f9d5b9217955098d4991b96fe19877064
Yong Ler <tyongler.tang@gmail.com>
2021-08-04 03:05:12 +0800

    DONE HOMEPAGE

--- citi_hackathon/lib/const/colors.dart
+++ citi_hackathon/lib/const/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Color blue = Color(0xFF507FDC);
-const Color white = Color(0xFFE5E5E5);
+const Color white = Color(0xFFEFEFEF);
 const Color black = Color(0xFF09051C);
-const Color containerWhite = Color(0xFFF4F4F4);
+const Color containerWhite = Color(0xFFF3ECEB);
 const Color searchbarGrey = Color(0xFFc9c7c7);
 const Color grey = Color(0xFF797979);
